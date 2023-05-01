5f467c467e7ca6a08b42f325760db4cd41f28940
Angel Gonzalez <aangel.gg@hotmail.com>
2023-05-01 21:01:49 +0200

    advertencia de prototipo

--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -6,11 +6,16 @@ import FooterComponent from '../../common/components/FooterComponent'
 
 const HomeScreen = () => {
   return (
-    <div className='max-w-[1024px] min-h-screen mx-auto px-3'>
-      <HeaderComponent />
-      <HeroComponent />
-      <CreateSpeechComponent />
-      <FooterComponent />
+    <div className='max-w-[1024px] min-h-screen mx-auto'>
+      <div className='w-full bg-orange-300 p-1'>
+        <p className='text-xs text-center'>Esta página es un prototipo y puede presentar problemas de funcionamiento.</p>
+      </div>
+      <div className='px-3'>
+        <HeaderComponent />
+        <HeroComponent />
+        <CreateSpeechComponent />
+        <FooterComponent />
+      </div>
     </div>
   )
 }
